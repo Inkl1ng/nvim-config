6b836e1687a5c2239bffb4d5bd9717803961a93b
Inkl1ng <scottbot2018@gmail.com>
2023-05-14 15:25:48 -0700

    Add new ruler

--- init.lua
+++ init.lua
@@ -7,8 +7,8 @@ vim.cmd("set softtabstop=4")
 -- toggle relative line numbers
 vim.cmd("set relativenumber")
 
--- toggle rulers for columns 72, 80, and 120
-vim.cmd("set colorcolumn=72,80,120")
+-- toggle rulers for columns 72, 80, 100 and 120
+vim.cmd("set colorcolumn=72,80,100,120")
 
 -- switch between .cpp and .hpp with <A-m>
 -- works both ways
