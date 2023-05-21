f7d820a81d4eefb1d15000810163fe30a4afd446
Inkl1ng <102632899+Inkl1ng@users.noreply.github.com>
2023-05-21 11:07:40 -0700

    Add command to exit terminal mode with <Esc>

--- mappings.lua
+++ mappings.lua
@@ -9,6 +9,9 @@ M.abc = {
     ["<leader>gg"] = {":LazyGit<Enter>", "Open LazyGit"},
     ["<Enter>"] = {"o<Esc>", "New Line Below"},
     ["<A-Enter>"] = {"O<Esc>", "New Line Above"}
+  },
+  t = {
+    ["<Esc>"] = { vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true), "escape terminal mode" },
   }
 }
 
