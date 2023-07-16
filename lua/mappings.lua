ce6ca7b938b77d15d0db0ea2c1b95f0fd7aa3006
Inkl1ng <scottbot2018@gmail.com>
2023-07-15 21:00:26 -0700

    Add mapping to open Themery and set all commands as silent and noremap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,15 +1,18 @@
 local map = vim.api.nvim_set_keymap
 
 -- Telescope file browser
-map('n', '<leader>fb', ':Telescope file_browser<cr>', { noremap = true })
+map('n', '<leader>fb', ':Telescope file_browser<cr>', { noremap = true, silent = true })
 
 -- buffer management
-map('n', '<leader>[', ':bprevious<cr>', {})
-map('n', '<leader>]', ':bnext<cr>', {})
-map('n', '<leader>\\', ':bd<cr>', {})
+map('n', '<leader>[', ':bprevious<cr>', { noremap = true, silent = true })
+map('n', '<leader>]', ':bnext<cr>', { noremap = true, silent = true })
+map('n', '<leader>\\', ':bd<cr>', { noremap = true, silent = true })
 
 -- exit terminal insert mode
-vim.keymap.set('t', '<c-x>', '<C-\\><c-n>', {})
+vim.keymap.set('t', '<c-x>', '<C-\\><c-n>', { noremap = true, silent = true })
 
 -- open lazygit
 map('n', '<leader>gg', ':LazyGit<cr>', { noremap = true, silent = true })
+
+-- open Themery
+map('n', '<leader>Th', ':Themery<cr>', { noremap = true, silent = true })
