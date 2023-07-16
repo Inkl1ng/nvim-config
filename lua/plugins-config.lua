458043447e3f063806cdc6ff4ab267e0e0675496
Inkl1ng <scottbot2018@gmail.com>
2023-07-15 20:50:58 -0700

    Change seperator style to slant

--- lua/plugins-config.lua
+++ lua/plugins-config.lua
@@ -5,7 +5,6 @@ vim.opt.termguicolors = true
 require("bufferline").setup {
     options = {
         numbers = 'ordinal',
-        color_icons = false,
-        separator_style = "slant"
+        color_icons = false
     }
 }
