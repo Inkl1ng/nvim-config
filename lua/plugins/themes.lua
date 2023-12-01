1a92f74a6e4908680f9143c0a70a7c4028b0026b
Inkl1ng <102632899+Inkl1ng@users.noreply.github.com>
2023-11-30 20:09:27 -0800

    Add files via upload

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,43 @@
+local plugins =  {
+    -- themery (theme manager)
+    {
+        'zaldih/themery.nvim',
+        config = function()
+            require("themery").setup({
+                themes = {
+                    { name = 'VSCode', colorscheme = 'vscode' },
+                    { name = 'JetBrains dracula', colorscheme = 'darcula-solid'},
+                    { name = 'JetBrains Fleet', colorscheme = 'fleet' },
+                    { name = 'Atom One Dark', colorscheme = 'onedark'},
+                    { name = 'Dracula', colorscheme = 'dracula' },
+                    { name = 'Dracula soft', colorscheme = 'dracula-soft' },
+                },
+                themeConfigFile = "~/.config/nvim/lua/theme.lua",
+                livePreview = true
+            })
+        end
+    },
+
+    -- themes
+    {
+        'Mofiqul/vscode.nvim',
+        {
+            'briones-gabriel/darcula-solid.nvim',
+            dependencies = { 'rktjmp/lush.nvim' }
+        },
+        {
+            'navarasu/onedark.nvim',
+            config = function()
+                require('onedark').setup {
+                    -- options: dark, darker, cool, deep, warm, warmer, light
+                    style = 'warmer'
+                }
+                require('onedark').load()
+            end
+        },
+        'Mofiqul/dracula.nvim',
+        'felipeagc/fleet-theme-nvim'
+    }
+}
+
+return plugins
