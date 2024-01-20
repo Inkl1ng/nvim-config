76e783fcc7be1605acf10df91ed69f25625a93cf
Inkl1ng <scottbot2018@gmail.com>
2024-01-19 18:18:55 -0800

    more refactoring

--- init.lua
+++ init.lua
@@ -1,32 +1,19 @@
 -- set leader key to space, makes sure key mappings are correct
 vim.g.mapleader =  " "
 vim.g.maplocalleader = " "
+
 -- disable netrw
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
--- lazy.nvim setup
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
-
-require("lsp-config")
-require("setup")
-require("options")
-require("mappings")
-require("startup-screen")
-require("telescope-config")
+-- config
+require("config.lazy")
+require("config.commander")
+require("config.options")
+require("config.telescope")
+require("config.bufferline")
+require("config.startup")
+require("config.themes")
 
 -- Themery block
 -- This block will be replaced by Themery.
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,64 +1,15 @@
 local plugins =  {
     -- themery (theme manager)
-    {
-        'zaldih/themery.nvim',
-        config = function()
-            require("themery").setup({
-                themes = {
-                    {
-                        name = 'VSCode',
-                        colorscheme = 'vscode'
-                    },
-                    {
-                        name = 'JetBrains dracula',
-                        colorscheme = 'darcula-solid'
-                    },
-                    {
-                        name = 'JetBrains Fleet',
-                        colorscheme = 'fleet',
-                        after = [[
-                            vim.cmd("highlight ColorColumn guibg=#373737")
-                        ]]
-                    },
-                    {
-                        name = 'Atom One Dark',
-                        colorscheme = 'onedark'
-                    },
-                    {
-                        name = 'Dracula',
-                        colorscheme = 'dracula'
-                    },
-                    {
-                        name = 'Dracula soft',
-                        colorscheme = 'dracula-soft'
-                    },
-                },
-                themeConfigFile = "~/.config/nvim/init.lua",
-                livePreview = true
-            })
-        end
-    },
+    "zaldih/themery.nvim",
 
     -- themes
+    "Mofiqul/vscode.nvim",
     {
-        'Mofiqul/vscode.nvim',
-        {
-            'briones-gabriel/darcula-solid.nvim',
-            dependencies = { 'rktjmp/lush.nvim' }
-        },
-        {
-            'navarasu/onedark.nvim',
-            config = function()
-                require('onedark').setup {
-                    -- options: dark, darker, cool, deep, warm, warmer, light
-                    style = 'warmer'
-                }
-                require('onedark').load()
-            end
-        },
-        'Mofiqul/dracula.nvim',
-        'felipeagc/fleet-theme-nvim'
-    }
+        "briones-gabriel/darcula-solid.nvim",
+        dependencies = { "rktjmp/lush.nvim" }
+    },
+    "Mofiqul/dracula.nvim",
+    "felipeagc/fleet-theme-nvim"
 }
 
 return plugins
